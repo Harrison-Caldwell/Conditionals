b45bc2ace84b86274694cfa952f49e96b8a4cf99
Harrison Caldwell <h.cald16@gmail.com>
2021-10-04 12:30:18 -0300

    Update Program.cs

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -4,9 +4,21 @@ namespace Conditionals
 {
     class Program
     {
+        static int Score = 0;
+        static int lives = 3;
+        static bool oneUpGained = false;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (oneUpGained == false)
+            {
+                if (Score > 1000000)
+                {
+                    lives = lives + 1;
+                    oneUpGained = true;
+                }
+            }
+
         }
     }
 }
